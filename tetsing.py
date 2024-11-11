9e5bc7ebcfc0389c8c02f904385d1cfd38517103
tmclough <72697891+tmclough@users.noreply.github.com>
2024-11-11 09:10:39 -0500

    Update and rename tetsing,py to tetsing.py

--- tetsing.py
+++ tetsing.py
@@ -4,8 +4,22 @@ api_key=MY_GITHUB_TOKEN=12345abcde67890fg
 # Example of a GitHub Personal Access Token (PAT)
 github_pat=ghp_16vcZ4l4Q09lEKj0FSgfJHV1Dtp45E85q1t
 
+AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE"
+
+
 # Example of an AWS Access Key
 aws_access_key_id=AKIAIOSFODNN7EXAMPLE
+
+https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktY3VzdG9tZXI...
+-----END OPENSSH PRIVATE KEY-----
+
+DB_USERNAME="admin"
+DB_PASSWORD="P@ssw0rd123!"
+
+AIzaSyB1V-DdQsJ5URcQJ9LXYwOlu4zZmBkh6zA
 
 # Example of a database password
 db_password=secretpassword123
